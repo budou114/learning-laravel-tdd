b599057dea21b4f8aa729b77bdb34d977e835f85
budou114 <58349030+budou114@users.noreply.github.com>
2022-10-16 14:49:43 +0900

    リファクタリング

--- app/Models/VacancyLevel.php
+++ app/Models/VacancyLevel.php
@@ -18,14 +18,11 @@ class VacancyLevel extends Model
 
     public function mark(): string
     {
-        if ($this->remainingCount === 0) {
-            return '×';
-        }
-        if ($this->remainingCount < 5) {
-            return '△';
-        }
+        $marks = ['empty' => '×', 'few' => '△', 'enough' => '◎'];
+        $slug = $this->slug();
+        assert(isset($marks[$slug]), new \DomainException('invalid slug value.'));
 
-        return '◎';
+        return $marks[$slug];
     }
 
     public function slug(): string
