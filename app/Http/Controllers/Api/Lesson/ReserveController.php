e34986de2510053a747d2624d3569fa0c5211c34
budou114 <58349030+budou114@users.noreply.github.com>
2023-01-22 15:40:05 +0900

    SPA のフィーチャーテスト(レッドフェーズ)

--- app/Http/Controllers/Api/Lesson/ReserveController.php
+++ app/Http/Controllers/Api/Lesson/ReserveController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Lesson;
 use App\Http\Controllers\Controller;
 use App\Models\Lesson;
 use App\Models\Reservation;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +15,12 @@ class ReserveController extends Controller
     public function __invoke(Lesson $lesson)
     {
         $user = Auth::user();
+
+        try {
+            $user->canReserve($lesson);
+        } catch (Exception $e) {
+            return response()->json(['error' => '予約できません。:' . $e->getMessage()], Response::HTTP_CONFLICT);
+        }
         $reservation = Reservation::create(['lesson_id' => $lesson->id, 'user_id' => $user->id]);
 
         return response()->json($reservation, Response::HTTP_CREATED);
--- tests/Feature/Http/Controllers/Api/Lesson/ReserveControllerTest.php
+++ tests/Feature/Http/Controllers/Api/Lesson/ReserveControllerTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Response;
 use Tests\TestCase;
 use App\Models\Lesson;
+use App\Models\Reservation;
 use Tests\Factories\Traits\CreatesUser;
 
 class ReserveControllerTest extends TestCase
@@ -30,5 +31,26 @@ class ReserveControllerTest extends TestCase
             'lesson_id' => $lesson->id,
             'user_id' => $user->id,
         ]);
+    }
+
+    public function testInvoke_異常系()
+    {
+        $lesson = Lesson::factory()->create(['capacity' => 1]);
+        $lesson->reservations()->save(Reservation::factory()->make());
+        $user = $this->createUser();
+        $this->actingAs($user, 'api');
+
+        $response = $this->postJson("/api/lessons/{$lesson->id}/reserve");
+        $response->assertStatus(Response::HTTP_CONFLICT);
+        $response->assertJsonStructure(['error']);
+
+        $error = $response->json('error');
+        $this->assertStringContainsString('予約できません。', $error);
+
+        $this->assertDatabaseMissing('reservations', [
+            'lesson_id' => $lesson->id,
+            'user_id' => $user->id,
+        ]);
+        
     }
 }
